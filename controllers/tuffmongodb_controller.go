c855d590f09742423e979f83d8fd6ec878362a94
Kursad Yildirim <kyildiri@kyildiri.remote.csb>
2023-03-09 16:15:50 +0100

    using manisfests and deployment

--- controllers/tuffmongodb_controller.go
+++ controllers/tuffmongodb_controller.go
@@ -1,19 +1,3 @@
-/*
-Copyright 2023.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
 package controllers
 
 import (
@@ -25,6 +9,7 @@ import (
 	ctrllog "sigs.k8s.io/controller-runtime/pkg/log"
 
 	appv1alpha1 "github.com/kursad-yildirim/kubernetes-mongodb-operator/api/v1alpha1"
+	"github.com/kursad-yildirim/kubernetes-mongodb-operator/manifests/deployment"
 
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
@@ -44,32 +29,17 @@ type TuffMongoDBReconciler struct {
 //+kubebuilder:rbac:groups=app.1k.local,resources=tuffmongodbs/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=app.1k.local,resources=tuffmongodbs/finalizers,verbs=update
 
-// Reconcile is part of the main kubernetes reconciliation loop which aims to
-// move the current state of the cluster closer to the desired state.
-// TODO(user): Modify the Reconcile function to compare the state specified by
-// the TuffMongoDB object against the actual cluster state, and then
-// perform operations to make the cluster state reflect the state specified by
-// the user.
-//
-// For more details, check Reconcile and its Result here:
-// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.13.0/pkg/reconcile
 func (r *TuffMongoDBReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
 	log := ctrllog.FromContext(ctx)
 
-	// Fetch the Mongodb.db instance
 	instance := &appv1alpha1.TuffMongoDB{}
 	err := r.Get(context.TODO(), req.NamespacedName, instance)
 	if err != nil {
 		if errors.IsNotFound(err) {
-			// Request object not found, could have been deleted after reconcile request.
-			// Owned objects are automatically garbage collected. For additional cleanup logic use finalizers.
-			// Return and don't requeue
 			return ctrl.Result{}, nil
 		}
-		// Error reading the object - requeue the request.
 		return ctrl.Result{}, err
 	}
-	// List all pods owned by this MicroMongo instance
 	tuffMongoDB := instance
 	mongoPodList := &corev1.PodList{}
 	tmLabels := map[string]string{
@@ -81,7 +51,6 @@ func (r *TuffMongoDBReconciler) Reconcile(ctx context.Context, req ctrl.Request)
 	if err = r.List(context.TODO(), mongoPodList, tmListOptions); err != nil {
 		return ctrl.Result{}, err
 	}
-	// Count the pods that are pending or running as available
 	var availableMongoPods []corev1.Pod
 	for _, pod := range mongoPodList.Items {
 		if pod.ObjectMeta.DeletionTimestamp != nil {
@@ -96,7 +65,6 @@ func (r *TuffMongoDBReconciler) Reconcile(ctx context.Context, req ctrl.Request)
 	for _, pod := range availableMongoPods {
 		availableMongoPodNames = append(availableMongoPodNames, pod.ObjectMeta.Name)
 	}
-	// Update the status if necessary
 	status := appv1alpha1.TuffMongoDBStatus{
 		MongoPodNames:          availableMongoPodNames,
 		MongoAvailableReplicas: numAvailableMongoPods,
@@ -126,9 +94,7 @@ func (r *TuffMongoDBReconciler) Reconcile(ctx context.Context, req ctrl.Request)
 
 	if numAvailableMongoPods < tuffMongoDB.Spec.MongoReplicas {
 		log.Info("Scaling up mongodb pods", "Currently available", numAvailableMongoPods, "Required replicas", tuffMongoDB.Spec.MongoReplicas)
-		// Define a new mongo pod object
 		mongoPod := newMongoPodForCR(tuffMongoDB)
-		// Set TuffMongoDB instance as the owner and controller
 		if err := controllerutil.SetControllerReference(tuffMongoDB, mongoPod, r.Scheme); err != nil {
 			return ctrl.Result{}, err
 		}
@@ -171,7 +137,6 @@ func newMongoPodForCR(cr *appv1alpha1.TuffMongoDB) (mongoPod *corev1.Pod) {
 	return
 }
 
-// SetupWithManager sets up the controller with the Manager.
 func (r *TuffMongoDBReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&appv1alpha1.TuffMongoDB{}).
